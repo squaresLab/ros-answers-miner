370823cdcb94448401afad657b57efbcd171104b
ChrisTimperley <christimperley@googlemail.com>
2020-06-29 10:46:42 -0400

    added placeholder cli module

---
+++ src/ros_answers_miner/cli.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import argparse
+
+
+def main() -> None:
+    # TODO #3 use argparse to provide a real command-line interface
+    print("This is the entrypoint to the ros-answers-miner executable")
