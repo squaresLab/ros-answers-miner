4af5be700c61321ab3d65efc2d8e2c50a626d717
Paulo Santos <pcanelasdossantos@gmail.com>
2022-11-09 13:37:41 -0500

    Finished parsing for a question

--- src/ros_answers_miner/parser.py
+++ src/ros_answers_miner/parser.py
@@ -3,7 +3,7 @@
 This module provides a simple interface for scraping all relevant information
 from a given ROS Answers URL.
 """
-__all__ = ('url_to_question',)
+__all__ = ('url_to_question', )
 
 import logging
 import requests
@@ -13,19 +13,8 @@ from typing import AbstractSet
 
 from ros_answers_miner.models import Question, Answer, Comment, User
 
+
 def build_link(url: str) -> str:
-    """Builds a link to a ROS Answers page.
-
-    Parameters
-    ----------
-    url : str
-        The relative URL of the ROS Answers page.
-
-    Returns
-    -------
-    str
-        The final URL of the ROS Answers page.
-    """
     return f'https://answers.ros.org/{url}'
 
 
@@ -34,8 +23,9 @@ def scrap_user(soup: BeautifulSoup) -> User:
 
     # From user_card, obtain the link with class js-avatar-box
     link = user_card[0].find_all("a", {"class": "js-avatar-box"})[0]['href']
-    
+
     return User(build_link(link))
+
 
 def scrap_answer(soup: BeautifulSoup) -> AbstractSet[Answer]:
 
@@ -44,30 +34,33 @@ def scrap_answer(soup: BeautifulSoup) -> AbstractSet[Answer]:
     # Get the div with the class post-body from the soup
     answers = soup.find_all("div", {"class": "post answer"})
 
-    # Get span with class next 
+    # Get span with class next
     next = soup.find("span", {"class": "next"})
-    next_link = next.find_all("a")[0]['href']
 
-    # Gather all the answers
-    while 'page=None' not in next_link:
+    if next:
+        next_link = next.find_all("a")[0]['href']
 
-        # Get the link from the next span
-        next_link = next.find_all("a")[0]['href']
-        
-        # Get the soup from the link
-        curr_soup: BeautifulSoup = BeautifulSoup(requests.get(build_link(next_link)).content, 'html.parser')
-        
-        # Get the div with the class post-body from the soup
-        answers += curr_soup.find_all("div", {"class": "post answer"})
-        
-        # Get span with class next 
-        next = curr_soup.find("span", {"class": "next"})
-        next_link = next.find_all("a")[0]['href']
+        # Gather all the answers
+        while 'page=None' not in next_link:
+
+            # Get the link from the next span
+            next_link = next.find_all("a")[0]['href']
+
+            # Get the soup from the link
+            curr_soup: BeautifulSoup = BeautifulSoup(
+                requests.get(build_link(next_link)).content, 'html.parser')
+
+            # Get the div with the class post-body from the soup
+            answers += curr_soup.find_all("div", {"class": "post answer"})
+
+            # Get span with class next
+            next = curr_soup.find("span", {"class": "next"})
+            next_link = next.find_all("a")[0]['href']
 
     # Get the accepted anser
-    accepted_answer = soup.find("div", {"class": "post answer accepted-answer"})
+    accepted_answer = soup.find("div",
+                                {"class": "post answer accepted-answer"})
 
-    print(len(answers))
     if accepted_answer is not None:
         answers.insert(0, accepted_answer)
 
@@ -90,7 +83,7 @@ def scrap_answer(soup: BeautifulSoup) -> AbstractSet[Answer]:
         date = ans.find("abbr", {"class": "timeago"})['title']
 
         # Get the votes of the answer
-        votes = ans.find("div", {"class": "vote-number"}).text
+        votes = int(ans.find("div", {"class": "vote-number"}).text)
 
         # Get the user of the answer
         user = ans.find("div", {"class": "user-info"}).find("a")['href'][1:]
@@ -103,11 +96,12 @@ def scrap_answer(soup: BeautifulSoup) -> AbstractSet[Answer]:
         answer = Answer(accepted, date, votes, user, content, comments)
 
         result.append(answer)
-    
+
     return result
 
+
 def scrap_comment(soup: BeautifulSoup) -> AbstractSet[Comment]:
-    
+
     # Get the list of comments from the comments div with class comment js-comment
     comms = soup.find("div", {"class": "comments"})
     comms = comms.find_all("div", {"class": "comment js-comment"})
@@ -115,12 +109,17 @@ def scrap_comment(soup: BeautifulSoup) -> AbstractSet[Comment]:
     comments = list()
 
     for comment in comms:
-        
+
         # Get the vote of the comment from the div with the class upvote js-score
         comment_vote = comment.find("div", {"class": "upvote js-score"}).text
 
+        if comment_vote == '':
+            comment_vote = 0
+
         # Get the comment content from the div with the class comment-body
-        comment_content = comment.find("div", {"class": "comment-body"}).text.strip()
+        comment_content = comment.find("div", {
+            "class": "comment-body"
+        }).text.strip()
 
         # Get the comment author from the a href with the class author
         comment_author = comment.find("a", {"class": "author"})['href'][1:]
@@ -130,18 +129,39 @@ def scrap_comment(soup: BeautifulSoup) -> AbstractSet[Comment]:
         comment_date = comment.find("abbr", {"class": "timeago"})['title']
 
         # Build the comment
-        comment = Comment(comment_date, comment_vote, comment_content, comment_author)
+        comment = Comment(comment_date, comment_vote, comment_content,
+                          comment_author)
         comments.append(comment)
-    
+
     return comments
 
 
 def scrap_question_info(soup: BeautifulSoup) -> tuple:
-    
-    # Get the div with the class post-body from the soup
-    question = soup.find_all("div", {"class": "post-body"})[0]
 
-    pass
+    # Get the date of the question from the abbr with the class timeago
+    date = soup.find("abbr", {"class": "timeago"})['title']
+
+    # Get the question number of votes from the div with the vote-number class
+    votes = int(soup.find("div", {"class": "vote-number"}).text)
+
+    # Get the question title
+    title = soup.find(
+        "h1", {"id": lambda l: l and l.startswith('js-question-title')})
+    title = title.find("div", {"class": "js-editable-content"}).text
+
+    # Get the question content. TODO: The images are not being scraped
+    content = soup.find("div",
+                        {"id": lambda l: l and l.startswith('js-post-body')})
+    content = content.find("div", {"class": "js-editable-content"}).text
+
+    # Get the list of tags in a question from the ul with id question-tags. The name is saved in the text
+    tags = soup.find("ul", {
+        "id": "question-tags"
+    }).find_all("a", {"class": "js-tag-name"})
+    tags = [tag.text for tag in tags]
+
+    return date, votes, title, content, tags
+
 
 def url_to_question(url: str) -> Question:
     """Produces a summary of the question at a given URL on ROS Answers.
@@ -152,23 +172,30 @@ def url_to_question(url: str) -> Question:
         If there is no question at the given URL.
     """
     logging.info(f'scraping question info at URL: {url}')
-    
+
     # Obtain the question page
-    soup: BeautifulSoup = BeautifulSoup(requests.get(url).content, 'html.parser')
+    soup: BeautifulSoup = BeautifulSoup(
+        requests.get(url).content, 'html.parser')
+
+    question_soup = soup.find("div", {"class": "post js-question"})
 
     # Obtain the question information
     user: User = scrap_user(soup)
     answers: AbstractSet[Answer] = scrap_answer(soup)
-    
-    for answer in answers:
-        print(answer)
-        print('-'*50)
-    print(len(answers))
-    assert False
-    comments: AbstractSet[Comment] = scrap_comment(soup)
-    
-    date, votes, views, title, content, tags = scrap_question_info(soup)
-    
+
+    # Obtain the comments for the question
+    comments: AbstractSet[Comment] = scrap_comment(question_soup)
+
+    date, votes, title, content, tags = scrap_question_info(question_soup)
+
+    # Obtain the div with class box statsWidget
+    stats = soup.find("div", {"class": "box statsWidget"})
+    views = stats.find_all("p")[1].text.strip().split(" ")[1]
+    views = int(views.replace(",", ""))
+
     logging.info(f'scraping question info at URL: {url} - done')
 
-    return Question(url, date, votes, views, user, title, content, tags, comments, answers)
+    question = Question(url, date, votes, views, user, title, content, tags,
+                        comments, answers)
+
+    return question
