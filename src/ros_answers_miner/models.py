55f0d15240bceec4d9e1f8e973a099ca0102f21a
Paulo Santos <64093960+pcanelas@users.noreply.github.com>
2023-04-05 17:22:03 -0400

    Update models.py

--- src/ros_answers_miner/models.py
+++ src/ros_answers_miner/models.py
@@ -6,17 +6,26 @@ Answers.
 __all__ = ('Answer', 'Comment', 'Question', 'User')
 
 from typing import AbstractSet
-from bs4.element import Tag
 
 import attr
 
 
-@attr.s(auto_attribs=True, frozen=True)
 class User:
     url: str
 
+    def __init__(self, url: str):
+        self.url = url
 
-@attr.s(auto_attribs=True)
+    def __key(self):
+        return (self.url)
+
+    def __hash__(self):
+        return hash(self.__key())
+
+    def __eq__(self, other):
+        return isinstance(other, User) and self.__key() == other.__key()
+
+
 class Comment:
     date: str
     votes: int
@@ -24,8 +33,22 @@ class Comment:
 
     user: User
 
+    def __init__(self, date, votes, content, user):
+        self.date = date
+        self.votes = votes
+        self.content = content
+        self.user = user
 
-@attr.s(auto_attribs=True)
+    def __key(self):
+        return (self.votes, self.content)
+
+    def __hash__(self):
+        return hash(self.__key())
+
+    def __eq__(self, other):
+        return isinstance(other, Comment) and self.__key() == other.__key()
+
+
 class Answer:
     accepted: bool
 
@@ -36,8 +59,24 @@ class Answer:
     content: str
     comments: AbstractSet[Comment]
 
+    def __init__(self, accepted, date, votes, user, content, comments):
+        self.accepted = accepted
+        self.date = date
+        self.votes = votes
+        self.user = user
+        self.content = content
+        self.comments = comments
 
-@attr.s(auto_attribs=True)
+    def __key(self):
+        return (self.accepted, self.votes, self.content)
+
+    def __hash__(self):
+        return hash(self.__key())
+
+    def __eq__(self, other):
+        return isinstance(other, Answer) and self.__key() == other.__key()
+
+
 class Question:
     # Default information about a question
     url: str
@@ -54,3 +93,25 @@ class Question:
     tags: AbstractSet[str]
     comments: AbstractSet[Comment]
     answers: AbstractSet[Answer]
+
+    def __init__(self, url, date, votes, views, user, title, content, tags, comments, answers):
+        self.url = url
+        self.date = date
+        self.votes = votes
+        self.views = views
+        self.user = user
+        self.title = title
+        self.content = content
+        self.tags = tags
+        self.comments = comments
+        self.answers = answers
+
+    # Contains code: AbstractSet[str]
+    def __key(self):
+        return (self.url, self.votes, self.views, self.title)
+
+    def __hash__(self):
+        return hash(self.__key())
+
+    def __eq__(self, other):
+        return isinstance(other, Question) and self.__key() == other.__key()
